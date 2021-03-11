a2e3b48b933072852a5d724d7970255fafa86155
MatveyTishin <MatveyTishin@yandex.ru>
2021-03-11 18:00:42 +0300

    Revert "fix by vera"
    This reverts commit fc8a44007ebd30bc87cfd61c8fd309835d65fb79.

--- script.js
+++ script.js
@@ -1,55 +1,53 @@
-var request = null;
-function CreateTablePosition() {    
-	var requestURL = 'http://localhost/data';
-	request = new XMLHttpRequest();
-	request.open('GET', requestURL);
-	request.responseType = 'json';
-	request.send();
-	request.onload = function() {
-		var parsle = request.response;
-		console.log(parsle);
-		console.log(parsle['status'])
-		var mass = parsle["body"];
-		console.log(mass);
-		var tables_html = `
-		<table cellspacing="0" id='tablo'>
-		<tr>  
+        var requestURL = 'http://localhost/data';
+        var request = new XMLHttpRequest();
+        request.open('GET', requestURL);
+        request.responseType = 'json';
+        request.send();
+        request.onload = function() {
+            var parsle = request.response;
+            console.log(parsle);
+            console.log(parsle['status'])
+            var mass = parsle["body"];
+            console.log(mass);
+            var tables_html = `
+            <table cellspacing="0" id='tablo'>
+            <tr>  
 
-		<th class="checkbox"> &#9898; </th> <!-- ChekBox -->
-		<th >Наименнование</th>
-		<th >Местоположение</th>
-		<th class="weight">Размер(Д*Ш*В)</th>
-		<th class="weight">Вес</th>
-		<th class="Comment">Комментарий</th>
-		<th >UUID</th>
+            <th class="checkbox"> &#9898; </th> <!-- ChekBox -->
+            <th >Наименнование</th>
+            <th >Местоположение</th>
+            <th class="weight">Размер(В*Ш*Г)</th>
+            <th class="weight">Вес</th>
+            <th class="Comment">Комментарий</th>
+            <th >UUID</th>
 
-		</tr>`;
-		for (let index = 0; index < mass.length; index++) {
-			const element = mass[index];
-			var Comm = element["Comments"];
-			if (Comm == 'NULL') {
-				Comm = '';
-			}
-			tables_html+=`
-			<tr>
-			<td class="checkbox"> <input type="checkbox" id="check-box-${index}" name="a" value= ${element["UUID"]} > </td>
-			<td >`+element["Name"]+`</td>
-			<td >`+element["PositionCell"]+`</td>
-			<td class="weight">`+element["Height"] + '*' + element["Width"] + '*' + element["Depth"]+`</td>
-			<td class="weight">`+element["Weight"]+`</td>
-			<td class="Comment">`+Comm+`</td>
-			<td >`+element["UUID"]+`</td>
+            </tr>`;
+            for (let index = 0; index < mass.length; index++) {
+                const element = mass[index];
+				var Comm = element["Comments"];
+				if (Comm == 'NULL') {
+					Comm = '';
+				}
+                tables_html+=`
+                <tr>
+                <td class="checkbox"> <input type="checkbox" name="a" value="${element["UUID"]}" > </td>
+                <td >`+element["Name"]+`</td>
+                <td >`+element["PositionCell"]+`</td>
+                <td class="weight">`+element["Height"] + '*' + element["Width"] + '*' + element["Depth"]+`</td>
+                <td class="weight">`+element["Weight"]+`</td>
+                <td class="Comment">`+Comm+`</td>
+                <td >`+element["UUID"]+`</td>
 
-			</tr>
-			`;
-		}
-		tables_html+='</table>';
-		console.log(tables_html);
-		document.getElementsByClassName('right')[0].innerHTML = tables_html;
-		
-		var chekcboxes  = `
+                </tr>
+                `;
+            }
+            tables_html+='</table>';
+            console.log(tables_html);
+            document.getElementsByClassName('right')[0].innerHTML = tables_html;
+            
+            var chekcboxes  = `
 
-	`}
+            `
 
 ///populateHeader(parsle);
 ///showParsle(parsle);
@@ -65,37 +63,19 @@ if (parsle ['status'] == 200) console.log('ошибка!');
             "iuuid",
             "icomment"
         ]
-    let values_k = [
-            "iname",
-            "ilength",
-            "iwidth",
-            "iheigth",
-            "iweigth",
-            //"iuuid",
-            "icomment"
-        ]
-
     function checkValues() {
-        for(j = 0; j <= values_k.length - 2; j++) {
-            if(document.getElementById(values_k[j]).value == "") {
+        for(j = 0; j <= values.length - 1; j++) {
+            if(document.getElementById(values[j]).value == "") {
                 return false;
             }
         }
         return true;
     }
 
-    function generateUUID() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
-    }
-
     function pasteValues(data) {
-        for(k = 0; k <= values_k.length - 1; k++) {
-            document.getElementById(`${values_k[k]}-set-${data}`).innerHTML = document.getElementById(values_k[k]).value;
-            document.getElementById(`iuuid-set-${data}`).innerHTML = generateUUID();
-            document.getElementById(values_k[k]).value = "";
+        for(k = 0; k <= values.length - 1; k++) {
+            document.getElementById(`${values[k]}-set-${data}`).innerHTML = document.getElementById(values[k]).value;
+            document.getElementById(values[k]).value = "";
         }
     }
 
@@ -143,10 +123,8 @@ if (parsle ['status'] == 200) console.log('ошибка!');
 			else {
 				FlagBool = true;
 			}
-			OutData += '{"Name": "'+ getValueTable("iname", j) +'","UUID": "'+ getValueTable("iuuid", j) +'","Height": '+ getValueTable("iheigth", j) +',"Width": '+ getValueTable("iwidth", j) +',"Depth": '+ getValueTable("ilength", j) 
-			+',"Weight": '+ getValueTable("iweigth", j) + ',"Comment": "'+ getValueTable("icomment", j) +'"}';
+			OutData += '{"Name": "'+ getValueTable("iname", j) +'","UUID": "'+ getValueTable("iuuid", j) +'","Height": '+ getValueTable("iheigth", j) +',"Width": '+ getValueTable("iwidth", j) +',"Depth": '+ getValueTable("ilength", j) +',"Weight": '+ getValueTable("iweigth", j) +'}';
 		}
-		console.log(OutData);
 		OutData += ']}';
 		$.ajax({
 				type: "POST",
@@ -156,67 +134,9 @@ if (parsle ['status'] == 200) console.log('ошибка!');
 				},
 				dataType:"json",
 				data: OutData,
-				success: location.replace("#reload_tables")
+				//data: '{"body": [{"Name": "'+ getValueTable("iname", j) +'","UUID": "'+ getValueTable("iuuid", j) +'","Height": '+ getValueTable("iheigth", j) +',"Width": '+ getValueTable("iwidth", j) +',"Depth": '+ getValueTable("ilength", j) +',"Weight": '+ getValueTable("iweigth", j) +'}]}',
+				success: function(data){
+					location.replace("#close_okno");
+				}
 		});
-	setTimeout(() => {CreateTablePosition();}, 400);
-    }
-
-    /*function drawingResult(data) {
-        let result = `{ ${data} }`;
-        return result;
-    }
-
-    function givePos() {
-        var parsle = request.response;
-        var mass = parsle["body"];
-        
-        let codevalues = 0;
-        console.log(mass.length);
-        for(k = 0; k < mass.length; k++) {
-            $(`#check-box-${k}`).on('click', function () {
-                if ( $(this).is(':checked') ) {
-                    codevalues[k] = document.getElementById(`check-box-${k}`).getAttribute('value');
-                    console.log(codevalues[k]);
-                }
-            });
-        }
-    }*/
-
-    function returnCheckBox(k) {
-        return document.getElementById(`check-box-${k}`);
-    }
-
-    function GivingPos(){
-        var parsle = request.response;
-        var mass = parsle["body"];
-
-        var gotCheckBox = '{"UUID": [';
-        var GivingFlag = false
-        for (var index = 0; index < mass.length; index++){
-                let chk = returnCheckBox(index);
-
-                if (chk.checked){
-                    s = chk.value;
-                    s =  String(s);
-                    if (GivingFlag) {
-                        gotCheckBox += ', ';
-                    }
-                    else {
-                        GivingFlag = true;
-                    }
-                    gotCheckBox += '"' + s + '"';
-                }
-        }
-        gotCheckBox += ']}';
-        console.log(gotCheckBox);
-        $.ajax({
-                    type: "POST",
-                    url: "http://localhost/api/issue",
-                    headers: {
-                        "Accept":" application/json"
-                    },
-                    dataType:"json",
-                    data: gotCheckBox
-		});
-		setTimeout(() => {CreateTablePosition();}, 400);
     }
